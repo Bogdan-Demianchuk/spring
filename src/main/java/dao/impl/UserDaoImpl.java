c900f0cb2d98f824f26b559b5d90657aea426229
bogdan <61113649+Bogdan-Demianchuk@users.noreply.github.com>
2020-06-03 18:09:52 +0300

    solution

---
+++ src/main/java/dao/impl/UserDaoImpl.java
@@ -0,0 +1,52 @@
+package dao.impl;
+
+import dao.UserDao;
+import exception.DataProcessingException;
+import java.util.List;
+import model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    private final SessionFactory sessionFactory;
+
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add user into db.", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<User> listUsers() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<User> query = session.createQuery("from User", User.class);
+            return query.list();
+        } catch (Exception e) {
+            throw new DataProcessingException("Could not get list of users from DB.", e);
+        }
+    }
+}
